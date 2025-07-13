24902635df3c2eaed6b343de383dcb56bf1d3578
MxS3 <elsanea2019@gmail.com>
2025-07-13 06:17:13 +0300

    Update ristoanime.js

--- ristoanime.js
+++ ristoanime.js
@@ -1,8 +1,25 @@
+function decodeHTMLEntities(text) {
+    text = text.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
+    const entities = {
+        '&quot;': '"',
+        '&amp;': '&',
+        '&apos;': "'",
+        '&lt;': '<',
+        '&gt;': '>'
+    };
+    for (const entity in entities) {
+        text = text.replace(new RegExp(entity, 'g'), entities[entity]);
+    }
+    return text;
+}
+
+function removeArabic(text) {
+    return text.replace(/[\u0600-\u06FF]/g, '').replace(/\s+/g, ' ').trim();
+}
+
 function searchResults(html) {
     const results = [];
-
     const itemBlocks = html.match(/<div class="MovieItem">[\s\S]*?<h4>(.*?)<\/h4>[\s\S]*?<\/a>/g);
-
     if (!itemBlocks) return results;
 
     itemBlocks.forEach(block => {
@@ -11,63 +28,53 @@ function searchResults(html) {
         const imgMatch = block.match(/background-image:\s*url\(([^)]+)\)/);
 
         if (hrefMatch && titleMatch && imgMatch) {
-            const href = hrefMatch[1].trim();
-            const title = titleMatch[1].trim();
-            const image = imgMatch[1].trim();
-
-            results.push({ title, image, href });
+            results.push({
+                title: removeArabic(titleMatch[1].trim()),
+                image: imgMatch[1].trim(),
+                href: hrefMatch[1].trim()
+            });
         }
     });
 
-    console.log(results);
     return results;
 }
 
 function extractDetails(html) {
-    const details = [];
+    const details = {};
 
     const descriptionMatch = html.match(/<p[^>]*>(.*?)<\/p>/s);
-    let description = descriptionMatch 
+    details.description = descriptionMatch 
         ? decodeHTMLEntities(descriptionMatch[1].trim()) 
         : 'N/A';
 
     const aliasMatch = html.match(/<li>\s*<div class="icon">\s*<i class="far fa-clock"><\/i>\s*<\/div>\s*<span>\s*مدة العرض\s*:\s*<\/span>\s*<a[^>]*>\s*(\d+)\s*<\/a>/);
-    let alias = aliasMatch ? aliasMatch[1].trim() : 'N/A';
+    details.alias = aliasMatch ? aliasMatch[1].trim() : 'N/A';
 
     const airdateMatch = html.match(/<li>\s*<div class="icon">\s*<i class="far fa-calendar"><\/i>\s*<\/div>\s*<span>\s*تاريخ الاصدار\s*:\s*<\/span>\s*<a[^>]*?>\s*(\d{4})\s*<\/a>/);
-    let airdate = airdateMatch ? airdateMatch[1].trim() : 'N/A';
+    details.airdate = airdateMatch ? airdateMatch[1].trim() : 'N/A';
 
-    details.push({
-        description: description,
-        alias: alias,
-        airdate: airdate
-    });
+    details.episodes = extractEpisodes(html);
 
-    console.log(details);
     return details;
 }
 
 function extractEpisodes(html) {
     const episodes = [];
-
     const episodeRegex = /<a href="([^"]+)">\s*الحلقة\s*<em>(\d+)<\/em>\s*<\/a>/g;
     let match;
 
     while ((match = episodeRegex.exec(html)) !== null) {
         const href = match[1].trim() + "/watch/";
         const number = match[2].trim();
-
         episodes.push({
+            title: `Episode ${number}`,
             href: href,
             number: number
         });
     }
 
-    if (episodes.length > 0 && episodes[0].number !== "1") {
-        episodes.reverse();
-    }
-
-    console.log(episodes);
+    // ترتيب الحلقات
+    episodes.sort((a, b) => parseInt(a.number) - parseInt(b.number));
     return episodes;
 }
 
@@ -77,7 +84,6 @@ async function extractStreamUrl(html) {
     const multiStreams = { streams: [], subtitles: null };
 
     const matches = [...html.matchAll(/<iframe[^>]+src=["'](?:https?:)?\/\/sendvid\.com\/embed\/([^"']+)["'][^>]*>/gi)];
-
     if (!matches.length) return JSON.stringify(multiStreams);
 
     const lastId = matches[matches.length - 1][1];
@@ -95,24 +101,6 @@ async function extractStreamUrl(html) {
     });
 
     return JSON.stringify(multiStreams);
-}
-
-function decodeHTMLEntities(text) {
-    text = text.replace(/&#(\d+);/g, (match, dec) => String.fromCharCode(dec));
-    
-    const entities = {
-        '&quot;': '"',
-        '&amp;': '&',
-        '&apos;': "'",
-        '&lt;': '<',
-        '&gt;': '>'
-    };
-    
-    for (const entity in entities) {
-        text = text.replace(new RegExp(entity, 'g'), entities[entity]);
-    }
-
-    return text;
 }
 
 async function soraFetch(url, options = { headers: {}, method: 'GET', body: null }) {
@@ -135,4 +123,6 @@ function _0xCheck() {
     })(_0xB4F2()) : !1;
 }
 
-function _0x7E9A(_){return((___,____,_____,______,_______,________,_________,__________,___________,____________)=>(____=typeof ___,_____=___&&___[String.fromCharCode(...[108,101,110,103,116,104])],______=[...String.fromCharCode(...[99,114,97,110,99,105])],_______=___?[...___[String.fromCharCode(...[116,111,76,111,119,101,114,67,97,115,101])]()]:[],(________=______[String.fromCharCode(...[115,108,105,99,101])]())&&_______[String.fromCharCode(...[102,111,114,69,97,99,104])]((_________,__________)=>(___________=________[String.fromCharCode(...[105,110,100,101,120,79,102])](_________))>=0&&________[String.fromCharCode(...[115,112,108,105,99,101])](___________,1)),____===String.fromCharCode(...[115,116,114,105,110,103])&&_____===16&&________[String.fromCharCode(...[108,101,110,103,116,104])]===0))(_)}
+function _0x7E9A(_) {
+    return ((___,____,_____,______,_______,________,_________,__________,___________,____________)=>(____=typeof ___,_____=___&&___['length'],______=[...'cranci'],_______=___?[...___['toLowerCase']()] : [],(________='slice')&&_______['forEach']((_________,__________)=>(___________=_____.indexOf(_________)) >= 0 && _____.splice(___________,1)),____==='string' && _____===16 && _______.length===0))(_)
+}
